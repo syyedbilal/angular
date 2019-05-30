cd065d60a467005500b29b93ff5b34309562e34c
bilal ahmad <bilal.ahmad@laitkor.com>
2019-05-30 12:58:54 +0530

    angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
